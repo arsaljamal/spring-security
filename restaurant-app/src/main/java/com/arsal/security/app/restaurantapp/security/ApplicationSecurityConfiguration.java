de77950c2b2433ee2de7806629af05e01fe33896
arsaljamal <arsaljamal2348@gmail.com>
2021-11-17 20:28:47 +0500

    added jdbc authentication

--- restaurant-app/src/main/java/com/arsal/security/app/restaurantapp/security/ApplicationSecurityConfiguration.java
+++ restaurant-app/src/main/java/com/arsal/security/app/restaurantapp/security/ApplicationSecurityConfiguration.java
@@ -1,13 +1,35 @@
 package com.arsal.security.app.restaurantapp.security;
 
+import com.arsal.security.app.restaurantapp.auth.RestaurantDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private RestaurantDetailService restaurantDetailService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(restaurantDetailService);
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return  daoAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
