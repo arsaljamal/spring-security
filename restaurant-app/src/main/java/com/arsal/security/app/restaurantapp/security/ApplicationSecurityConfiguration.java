efa572ef7ca9b49d55d3ef27a3c06893b74ae099
arsaljamal <arsaljamal2348@gmail.com>
2021-11-17 17:47:54 +0500

    added in-memory authentication

--- restaurant-app/src/main/java/com/arsal/security/app/restaurantapp/security/ApplicationSecurityConfiguration.java
+++ restaurant-app/src/main/java/com/arsal/security/app/restaurantapp/security/ApplicationSecurityConfiguration.java
@@ -1,9 +1,18 @@
 package com.arsal.security.app.restaurantapp.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +27,14 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+        userDetailsList.add(User.withDefaultPasswordEncoder().username("arsal").password("123").roles("USER","ADMIN").build());
+        userDetailsList.add(User.withDefaultPasswordEncoder().username("jamal").password("124").roles("USER").build());
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
 }
